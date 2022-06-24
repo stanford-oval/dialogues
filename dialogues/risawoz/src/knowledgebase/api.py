73f4e82959b66078302aa07374ded7b2d6ef045f
mehrad <mehrad@stanford.edu>
2022-06-23 18:40:43 -0700

    Update api calling

--- dialogues/risawoz/src/knowledgebase/api.py
+++ dialogues/risawoz/src/knowledgebase/api.py
@@ -2,14 +2,10 @@ from pymongo import MongoClient
 
 from dialogues.risawoz.src.knowledgebase.en_zh_mappings import RisaWOZMapping
 
-mongodb_host = 'mongodb+srv://bitod:plGYPp44hASzGbmm@cluster0.vo7pq.mongodb.net/bilingual_tod?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
+mongodb_host = 'mongodb+srv://bitod:plGYPp44hASzGbmm@cluster0.vo7pq.mongodb.net/risawoz?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE'
 client = MongoClient(mongodb_host, authSource='admin')
 
-# risawoz db collections are under bilingual_tod; fix once we get around permission errors
-risawoz_db = client["bilingual_tod"]
-# for col in risawoz_db.list_collection_names():
-#     if '_risawoz' in col:
-#         risawoz_db.drop_collection(col)
+risawoz_db = client["risawoz"]
 risawoz_mapping = RisaWOZMapping()
 
 
@@ -21,7 +17,7 @@ def call_api(api_names, constraints=None, lang=None):
         knowledge[api] = {}
         domain_constraints = constraints[api]
         # db_name = api if api_map is None else api_map[api]
-        db_name = f'{risawoz_mapping._risawoz_API_MAP[api]}_{lang}_risawoz'
+        db_name = f'{risawoz_mapping._risawoz_API_MAP[api]}_{lang}'
         # cursor = db[api].find(domain_constraints).sort([("rating", pymongo.ASCENDING), ("_id", pymongo.DESCENDING)])
         cursor = risawoz_db[db_name].find(domain_constraints)
         domain_knowledge = []
