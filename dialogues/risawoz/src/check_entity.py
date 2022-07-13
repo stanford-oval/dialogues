3b41403d183cbb348f813efe56ef10e59ec5c8b8
mehrad <mehrad@stanford.edu>
2022-07-13 12:51:17 -0700

    Fix check entity

--- dialogues/risawoz/src/check_entity.py
+++ dialogues/risawoz/src/check_entity.py
@@ -1,10 +1,9 @@
 import argparse
 import os
-import sys
 
 import ujson
 
-from dialogues.risawoz.main import Risawoz
+from dialogues import Risawoz
 
 risawoz = Risawoz()
 
@@ -14,6 +13,7 @@ def get_input_output(dic):
     turn_ids = []
     inputs = []
     outputs = []
+    entities = []
     for turn in dic["data"]:
         if turn['train_target'] not in ["dst", "da"]:
             continue
@@ -22,37 +22,49 @@ def get_input_output(dic):
         turn_id = turn['turn_id']
         input = turn['input_text']
         output = turn['output_text']
-        state = risawoz.span2state(output)
+
         dial_ids.append(dial_id)
         turn_ids.append(turn_id)
         inputs.append(input)
-        outputs.append(state)
+        outputs.append(output)
 
-    return dial_ids, turn_ids, inputs, outputs
+        ents = []
+
+        if turn['train_target'] == 'dst':
+            output = risawoz.span2state(output)
+            for domain in output:
+                for (slot, value) in output[domain].items():
+                    for v in value['value']:
+                        ents.append(str(v))
+        else:
+            output = risawoz.span2action(output)
+            for domain in output:
+                for item in output[domain]:
+                    for v in item['value']:
+                        ents.append(str(v))
+
+        entities.append(ents)
+
+    return dial_ids, turn_ids, inputs, outputs, entities
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--directory",
-        type=str,
-        default='/Users/shiningsunnyday/Documents/GitHub/dialogues/dialogues/risawoz/data/Archive/',
-        help='directory of files to check',
-    )
+    parser.add_argument("--directory", type=str, help='directory of files to check')
     parser.add_argument("--setting", type=str, default='zh')
     parser.add_argument("--splits", type=str, nargs='+', default=['train', 'valid', 'test'])
     parser.add_argument("--version", type=int, default=5)
     args = parser.parse_args()
+
     for split in args.splits:
-        with open(os.path.join(args.directory, f"{split}_entity_check.txt"), 'w+') as f:
-            sys.stdout = f
-            for (dial_id, turn_id, input, output) in zip(
+        with open(os.path.join(args.directory, f"{split}_entity_check.tsv"), 'w+') as f:
+            for (dial_id, turn_id, input, output, ents) in zip(
                 *get_input_output(
                     ujson.load(open(os.path.join(args.directory, f'{args.setting}_{split}_v{args.version}.json')))
                 )
             ):
-                for domain in output:
-                    for (slot, value) in output[domain].items():
-                        for v in value['value']:
-                            if str(v) not in input:
-                                print(f"dial_id: {dial_id}\nturn_id: {turn_id}\n\" {v} \" not in {input}\n")
+                for ent in ents:
+                    if not ent:
+                        continue
+                    if ent not in input:
+                        f.write(f"{dial_id}\t{turn_id}\t{input}\t{output}\t{ent}\n")
