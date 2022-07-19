2cfcf4d771dc973ac2f5d4222dc819462c17d95a
Michael Sun <michaelsun18@yahoo.com>
2022-07-19 14:48:46 -0700

    replace outside quotes regex

---
+++ dialogues/risawoz/scripts/test_regex.py
@@ -0,0 +1,17 @@
+import re
+
+s = "( movie ) production_country_or_area 年代 equal_to \" 年代中国大陆 \" , 年代 equal_to \" 2010 年代 \" 年代"
+
+
+def replace_word(input, search, replace):
+    def replace_method(match):
+        if match.group(2) is None:
+            return match.group()
+        return match.group(2).replace(search, replace)
+
+    expr = re.compile("('[^']*'|\"[^\"]*\")|({})".format(search))
+    return re.sub(expr, replace_method, s)
+
+
+output = replace_word(s, "年代", "decade")
+print(output)
