1127aeccb94fd26331b094bb0f6988c88a888a86
mehrad <mehrad@stanford.edu>
2022-10-11 14:59:52 -0700

    check_entity: fix script

--- dialogues/risawoz/scripts/check_entity.py
+++ dialogues/risawoz/scripts/check_entity.py
@@ -67,10 +67,10 @@ if __name__ == "__main__":
     all_slots = list(risawoz.value_mapping.zh2en_SLOT_MAP.values())
 
     for split in args.splits:
-        with open(os.path.join(args.directory, f"{split}_entity_check_{args.version}.tsv"), 'w+') as f:
+        with open(os.path.join(args.directory, f"{split}_entity_check_{args.version}.tsv"), 'w') as f:
             for (dial_id, turn_id, input, output, ents) in zip(
                 *get_input_output(
-                    ujson.load(open(os.path.join(args.directory, f'{args.setting}_{split}_v{args.version}.json')))
+                    ujson.load(open(os.path.join(args.directory, f'{args.setting}_v{args.version}/{split}.json')))
                 )
             ):
                 for (slot, ent) in ents:
