483624e355e5bdfb3bdcd10ee6b14e9bff78e7ff
mehrad <mehrad@stanford.edu>
2022-10-21 19:04:00 -0700

    Fix tests

--- tests/risawoz/test_convert.py
+++ tests/risawoz/test_convert.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import os
+import random
+import subprocess
 
 from dialogues.risawoz.src.convert import build_dataset, build_db
 
@@ -15,6 +17,8 @@ parser.add_argument("--splits", nargs='+', default=['valid'])
 args = parser.parse_args()
 
 mongodb_host = "mongodb://localhost:27017/"
+
+random.seed('123')
 
 risawoz_db = build_db(
     db_json_path=os.path.join(*[args.root, f'database/db_{args.setting}']),
@@ -35,4 +39,11 @@ for split in args.splits:
     with open('converted_valid.json', 'w') as fout:
         json.dump(processed_data, fout, indent=4, ensure_ascii=False)
 
+    print(
+        subprocess.Popen(
+            "diff -u converted_valid.json ./tests/risawoz/data/converted_valid.json", shell=True, stdout=subprocess.PIPE
+        )
+        .stdout.read()
+        .decode()
+    )
     assert processed_data == gold_data
--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,8 @@
 # run all tests
 
-for dataset in bitod risawoz ; do
+for dataset in risawoz ; do
   for test_script in tests/${dataset}/*.py ; do
     echo "running ${test_script}"
-    python3 ${test_script}
+    python3 ${test_script} || exit 1
   done
 done
