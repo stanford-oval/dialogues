7f787ce6e0363b8e07e2e9bdac3b7faac06c9213
sung.balance <sung.balance@gmail.com>
2023-05-03 03:12:28 +0000

    update test cases

--- tests/run.sh
+++ tests/run.sh
@@ -1,7 +1,7 @@
 # run all tests
 for dataset in risawoz ; do
   for test_script in tests/${dataset}/*.py ; do
-    for setting in en zh fr hi enhi ; do
+    for setting in en zh fr hi enhi kr ; do
       echo "running ${test_script} with setting ${setting}"
       python3 ${test_script} --setting ${setting} || exit 1
     done
